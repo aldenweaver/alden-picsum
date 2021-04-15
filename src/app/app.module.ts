0ac60f648e2fbf036d1ddc4d0f9fbb26f230816d
aldenweaver <aldenweaver@users.noreply.github.com>
2021-04-15 11:46:11 -0700

    Angular app and one basic component up and running!
    Created a basic component containing just a header to be developed for listing pictures. I used this component in the app component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {PictureListComponent} from './picture-list/picture-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PictureListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/picture-list/picture-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'picture-list',
+  templateUrl: './picture-list.component.html',
+  styleUrls: ['./picture-list.component.css']
+})
+export class PictureListComponent {
+
+}
